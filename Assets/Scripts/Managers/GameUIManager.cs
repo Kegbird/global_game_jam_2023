2ed4b13827d942981037ddba5efc51e0132698cf
Nat1985 <andreanatale85@gmail.com>
2023-02-04 14:55:24 +0100

    SetInventorySpriteAtIndex

--- Assets/Scripts/Managers/GameUIManager.cs
+++ Assets/Scripts/Managers/GameUIManager.cs
@@ -11,18 +11,48 @@ public class GameUIManager : MonoBehaviour
     public Image _oxygen_bar;
     public Image _energy_bar;
     public Image _water_bar;
+
+    public Image[] _inventory_images = new Image[5];
+    public Sprite _sprite_test;
+
+    public void SetInventorySpriteAtIndex(int _index, Sprite _pickup_sprite)
+    {   
+        Color _color = _inventory_images[_index].color;
+        _inventory_images[_index] = _inventory_images[_index].GetComponent<Image>();
+        _color = _inventory_images[_index].color;
+        _color.a = 0;
+        _inventory_images[_index].color = _color;
+        _inventory_images[_index].sprite = _pickup_sprite;
+        _color.a = 1;
+        _inventory_images[_index].color = _color;
+    }
+
+    public void RemoveInventoryItem(int _index, Sprite pickup_sprite)
+    {
+        Color _color = _inventory_images[_index].color;
+        _color.a = 0;
+        _inventory_images[_index].color = _color;
+    }
+
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            SetInventorySpriteAtIndex(2, _sprite_test);
+        }
+    }
     
-    public void SetOxygenLevel(float _oxygen_perc)
+    public void OxygenLevel(float _oxygen_perc)
     {
         _oxygen_bar.fillAmount = _oxygen_perc;
     }
 
-    public void SetEnergyLevel(float _energy_perc)
+    public void EnergyLevel(float _energy_perc)
     {
         _energy_bar.fillAmount = _energy_perc;
     }
 
-    public void SetWaterLevel(float _water_perc)
+    public void WaterLevel(float _water_perc)
     {
         _water_bar.fillAmount = _water_perc;
     }
